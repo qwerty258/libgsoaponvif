7a7ee2f858f5b5c07380f9ae29a02fea1990fe23
qwerty258 <zyf26256@gmail.com>
2015-09-11 12:07:18 +0800

    discover all the device and find onvif

--- testProbeByMyself/testProbeByMyself.cpp
+++ testProbeByMyself/testProbeByMyself.cpp
@@ -70,61 +70,30 @@ int _tmain(int argc, _TCHAR* argv[])
         exit(0);
     }
 
-
-    //NetworkVideoTransmitter
-    //onvif://www.onvif.org
-
-
     receiveThreadParameter parameter;
     BOOL loop = TRUE;
     DWORD threadID;
     parameter.socketForProbe = &socketForProbe;
     parameter.bLoop = &loop;
 
+    result = _snprintf_s(pProbeMessage, 2048, _TRUNCATE, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:SOAP-ENC=\"http://www.w3.org/2003/05/soap-encoding\" xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" xmlns:wsdd=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\"><SOAP-ENV:Header><wsa:MessageID>urn:uuid:759a9a95-610f-40a9-bf26-fe2c2836ed50</wsa:MessageID><wsa:To SOAP-ENV:mustUnderstand=\"true\">urn:schemas-xmlsoap-org:ws:2005:04:discovery</wsa:To><wsa:Action SOAP-ENV:mustUnderstand=\"true\">http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</wsa:Action></SOAP-ENV:Header><SOAP-ENV:Body><wsdd:Probe><wsdd:Types></wsdd:Types><wsdd:Scopes></wsdd:Scopes></wsdd:Probe></SOAP-ENV:Body></SOAP-ENV:Envelope>");
+    if(-1 == result)
+    {
+        _tprintf(_T("_snprintf_s error"));
+        exit(0);
+    }
+
+    //result = _snprintf_s(pProbeMessage, 2048, _TRUNCATE, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:SOAP-ENC=\"http://www.w3.org/2003/05/soap-encoding\" xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" xmlns:wsdd=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\"><SOAP-ENV:Header><wsa:MessageID>urn:uuid:bc9fb550-1dd1-11b2-807c-c056e3fb5481</wsa:MessageID><wsa:To SOAP-ENV:mustUnderstand=\"true\">urn:schemas-xmlsoap-org:ws:2005:04:discovery</wsa:To><wsa:Action SOAP-ENV:mustUnderstand=\"true\">http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</wsa:Action></SOAP-ENV:Header><SOAP-ENV:Body><wsdd:Probe><wsdd:Types>Device</wsdd:Types><wsdd:Scopes></wsdd:Scopes></wsdd:Probe></SOAP-ENV:Body></SOAP-ENV:Envelope>");
+    //if(-1 == result)
+    //{
+    //    _tprintf(_T("_snprintf_s error"));
+    //    exit(0);
+    //}
+
     HANDLE hThread = CreateThread(NULL, 0, receiveThread, &parameter, 0, &threadID);
     if(NULL == hThread)
     {
         _tprintf(_T("CreateThread error, code: %d"), GetLastError());
-        exit(0);
-    }
-
-    result = _snprintf_s(pProbeMessage, 2048, _TRUNCATE, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:SOAP-ENC=\"http://www.w3.org/2003/05/soap-encoding\" xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" xmlns:wsdd=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\"><SOAP-ENV:Header><wsa:MessageID>urn:uuid:759a9a95-610f-40a9-bf26-fe2c2836ed50</wsa:MessageID><wsa:To SOAP-ENV:mustUnderstand=\"true\">urn:schemas-xmlsoap-org:ws:2005:04:discovery</wsa:To><wsa:Action SOAP-ENV:mustUnderstand=\"true\">http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</wsa:Action></SOAP-ENV:Header><SOAP-ENV:Body><wsdd:Probe><wsdd:Types>NetworkVideoTransmitter</wsdd:Types><wsdd:Scopes></wsdd:Scopes></wsdd:Probe></SOAP-ENV:Body></SOAP-ENV:Envelope>");
-    if(-1 == result)
-    {
-        _tprintf(_T("_snprintf_s error"));
-        exit(0);
-    }
-
-    result = sendto(socketForProbe, pProbeMessage, result, 0, (sockaddr*)&sockaddrMulticastAddrForOnvif, sizeof(sockaddr_in));
-    if(SOCKET_ERROR == result)
-    {
-        _tprintf(_T("sendto error, code: %d"), WSAGetLastError());
-        exit(0);
-    }
-
-    Sleep(5000);
-
-    loop = FALSE;
-
-    WaitForMultipleObjects(1, &hThread, TRUE, INFINITE);
-
-    CloseHandle(hThread);
-
-
-
-
-
-    hThread = CreateThread(NULL, 0, receiveThread, &parameter, 0, &threadID);
-    if(NULL == hThread)
-    {
-        _tprintf(_T("CreateThread error, code: %d"), GetLastError());
-        exit(0);
-    }
-
-    result = _snprintf_s(pProbeMessage, 2048, _TRUNCATE, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:SOAP-ENC=\"http://www.w3.org/2003/05/soap-encoding\" xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" xmlns:wsdd=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\"><SOAP-ENV:Header><wsa:MessageID>urn:uuid:bc9fb550-1dd1-11b2-807c-c056e3fb5481</wsa:MessageID><wsa:To SOAP-ENV:mustUnderstand=\"true\">urn:schemas-xmlsoap-org:ws:2005:04:discovery</wsa:To><wsa:Action SOAP-ENV:mustUnderstand=\"true\">http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</wsa:Action></SOAP-ENV:Header><SOAP-ENV:Body><wsdd:Probe><wsdd:Types>Device</wsdd:Types><wsdd:Scopes></wsdd:Scopes></wsdd:Probe></SOAP-ENV:Body></SOAP-ENV:Envelope>");
-    if(-1 == result)
-    {
-        _tprintf(_T("_snprintf_s error"));
         exit(0);
     }
 
@@ -194,10 +163,13 @@ DWORD WINAPI receiveThread(LPVOID lpParam)
         }
         else
         {
-            fwrite(buffer, bytesReceived, 1, pFile);
-            fwrite("\r\n\r\n\r\n", 6, 1, pFile);
+            if(NULL!=strstr(buffer,"onvif"))
+            {
+                fwrite(buffer, bytesReceived, 1, pFile);
+                fwrite("\r\n\r\n\r\n", 6, 1, pFile);
 
-            printf_s("%s\n\n\n", buffer);
+                printf_s("%s\n\n\n", buffer);
+            }
         }
     }
 
